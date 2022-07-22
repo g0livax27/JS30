2eff9a6cd99330f2f144187349fe2135a17e572a
Gerimae Oliva <gerimae.o@gmail.com>
2022-07-21 21:58:33 -0400

    commenting out script.js file

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -1,20 +1,20 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform')
-        return;
-    e.target.classList.remove('playing');
-}
+// function removeTransition(e) {
+//     if (e.propertyName !== 'transform')
+//         return;
+//     e.target.classList.remove('playing');
+// }
 
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+// function playSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
-    if (!audio) return;
+//     if (!audio) return;
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-}
+//     key.classList.add('playing');
+//     audio.currentTime = 0;
+//     audio.play();
+// }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transition', removeTransition));
-window.addEventListener('keydown', playSound);
+// const keys = Array.from(document.querySelectorAll('.key'));
+// keys.forEach(key => key.addEventListener('transition', removeTransition));
+// window.addEventListener('keydown', playSound);
